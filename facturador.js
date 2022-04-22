28aa1e06f33abf870cff29f590ea0e8ac19d457f
Kevin Arias <kevinariassistemas@gmail.com>
2022-04-21 20:22:11 -0500

    [feature]-data

--- facturador.js
+++ facturador.js
@@ -3,19 +3,19 @@ botonCalcular.onclick = function mostrar() {
     let producto = document.getElementById("listaProducto").value;
     let cantidad = document.getElementById("cantidad").value;
     console.log(producto)
-  
-    if(producto=="Cuaderno"){
-        let precio=30
+
+    if (producto == "Cuaderno") {
+        let precio = 30
         let descuento = 0.15
         console.log(precio * cantidad * descuento)
         compraTotal.push(precio * cantidad * descuento)
-    }else if(producto=="Lapicero"){
-        let precio=50
+    } else if (producto == "Lapicero") {
+        let precio = 50
         let descuento = 0.20
         console.log(precio * cantidad * descuento)
         compraTotal.push(precio * cantidad * descuento)
-    }else if(producto=="Regla"){
-        let precio=70
+    } else if (producto == "Regla") {
+        let precio = 70
         let descuento = 0.25
         console.log(precio * cantidad * descuento)
         compraTotal.push(precio * cantidad * descuento)
@@ -23,10 +23,16 @@ botonCalcular.onclick = function mostrar() {
     console.log("Total a pagar", compraTotal)
 
     let suma = 0;
+    let lista = "";
 
-    for(let i=0; i<compraTotal.length; i++){
+    for (let i = 0; i < compraTotal.length; i++) {
         suma += compraTotal[i];
+        lista += compraTotal[i] + "<br>"
     }
+    document.getElementById("datos").innerHTML = "<div>"+lista+"</div>"
+
+    document.getElementById("total").innerHTML = "<span>total a pagar: "+suma+"</span>"
+    console.log(compraTotal.length)
 
     console.log("Total a pagar", suma)
 }
